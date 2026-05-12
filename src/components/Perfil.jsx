61ed88dcf876d7de2d60c2c3f597b4712774b3f0
Nicolas8701 <168933211+Nicolas8701@users.noreply.github.com>
2026-05-12 15:59:47 -0300

    v2

---
+++ src/components/Perfil.jsx
@@ -0,0 +1,8 @@
+export const perfil = {
+  usuario: "catfighters2d",
+  nombre: "CatFighters 2D",
+  bio: "Modo gato no confirmado oficialmente... todavía.",
+  publicaciones: 12,
+  seguidores: 128,
+  seguidos: 8,
+}
---
+++ src/components/Posteo.jsx
@@ -0,0 +1,72 @@
+export const posteosBase = [
+  {
+    usuario: "catfighters2d",
+    caption: "Nuevo modo filtrado por accidente: peleas de gatitos.",
+    likes: 43,
+    fecha: "hace 2 horas",
+    comentarios: [
+      "Tank dice que sigue balanceado.",
+      "Esto ya es mejor que el meta actual."
+    ],
+  },
+  {
+    usuario: "fighters2d.oficial",
+    caption: "Probá el juego original acá: fighters2d.netlify.app",
+    likes: 87,
+    fecha: "hace 1 día",
+    link: "https://fighters2d.netlify.app",
+    esPromo: true,
+    comentarios: [
+      "La versión de gatos era necesaria.",
+      "Brawler main desde la beta."
+    ],
+  },
+    {
+    usuario: "AverageUroborosEnjoyer",
+    caption: "Fusilen a Chris Redfield y Sheva Alomar.",
+    likes: 2009,
+    fecha: "hace 67 años",
+    link: "https://residentevil.fandom.com/wiki/Game_Over_(RE5)",
+    esPromo: false,
+    comentarios: [
+      "No digas esas cosas. La violencia no esta bien",
+      "Albert Wesker aprueba este mensaje."
+    ],
+  },
+   {
+    usuario: "NikolaiDarrinsky",
+    caption: "Aguante el futbol wachin!!!",
+    likes: 3082009,
+    fecha: "hace 67 horas",
+    link: "https://emojiterra.com/emojis/laugh/",
+    esPromo: false,
+    comentarios: [
+      "Nooooooo aguante los de volley amigo...!",
+      "Que caranchos te pasaste de lanza webon😂"
+    ],
+  },
+  {
+    usuario: "MeowMeowMeow",
+    caption: "MeowMeow..Meow Meow Meow.",
+    likes: 46704451,
+    fecha: "hace 5 meses",
+    link: "https://emojiterra.com/emojis/laugh/",
+    esPromo: false,
+    comentarios: [
+      "Kitty",
+      "Que lindo gatito gato gatu que se escapo en estas tierras conocidas como instagram personal y personalizado por Tomas Lopez y Nicolas Frenkel. Todos los derechos reservados."
+    ],
+  },
+    {
+    usuario: "Xx_AsesinoCaballeros_xX",
+    caption: "Opino que el chocolate negro es el mejor.",
+    likes: 1258,
+    fecha: "hace 3 semanas",
+    link: "https://emojiterra.com/emojis/laugh/",
+    esPromo: false,
+    comentarios: [
+      "En mi opinion el blanco es mejor",
+      "Para mi depende pero un buen chocolate de leche es insuperable."
+    ],
+  },
+]
